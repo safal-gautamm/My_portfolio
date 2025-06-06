eb122335b3b467f636ee63604cfdb4af60c6e0b7
Safal Gautam <saphal106@gmail.com>
2025-06-06 12:01:27 +0545

    Final

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ hamburger.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   sidebar.classList.remove("active");
 });
+
+document.getElementById("getCV").addEventListener("click", () => {
+  const link = document.createElement('a');
+  link.href = 'assets/cv.pdf'; // change path if it's in /assets or /files
+  link.download = 'Safal_Gautam_CV.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link); // clean up!
+});
