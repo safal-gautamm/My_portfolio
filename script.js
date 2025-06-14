51d068d265f56535103571d0227c887a23f11b63
Safal Gautam <saphal106@gmail.com>
2025-06-14 15:34:01 +0545

    Well documented

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-
+// making query selector 
 let hamburger = document.querySelector(".hamburger");
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector(".close-btn");
 
+// eventlistener to check button is clicked
 hamburger.addEventListener("click", () => {
+
+  // Tweaking css on button click
   sidebar.classList.add("active");
 });
 
 closeBtn.addEventListener("click", () => {
+
+  // Tweaking css on button click
   sidebar.classList.remove("active");
 });
 
+// for 'my cv' button to enabling download
+// making query selector 
 document.getElementById("getCV").addEventListener("click", () => {
   const link = document.createElement('a');
   link.href = 'assets/cv.pdf'; // change path if it's in /assets or /files
